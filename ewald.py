4f3cc2947d9424cd25b7baa9ff1e57819b63d790
Jeff Thompson <jpthompson17@gmail.com>
2020-03-23 19:41:41 -0400

    Add files via upload

---
+++ ewald.py
@@ -0,0 +1,65 @@
+"""Ewald summation for Madelung energy
+"""
+
+from math import inf, isinf, pi, sqrt, exp, erfc
+from itertools import product
+
+def madelung(screening_length=inf, g_ewald=7.1, kmax=12, approx=False):
+    """Madelung constant for 1x1x1 Yukawa lattice with neutralizing background.
+
+    Keyword arguments:
+    screening_length -- the screening length (default inf)
+    g_ewald -- Ewald splitting parameter (default 7.1)
+    kmax -- range of reciprocal space sum (default 12)
+    approx -- whether to use closed-form approximation (default False)
+
+    See eqn (2.19) of Salin and Caillol, J. Chem. Phys. 113, 10459 (2000).
+    (alpha = 1/screening_length, beta = g_ewald)
+    """
+
+    if isinf(screening_length):
+        return -2.837297479
+    if screening_length == 0:
+        return 0
+
+    a = 1.0 / screening_length # screening wavenumber
+    a2 = a * a
+
+    # closed-form approximation
+    if approx:
+        r = 0.911544
+        r2 = r * r
+        return (4*pi/a2 * (a*r + 1 + a2*r2/3) - 1/r)*exp(-a*r) - 4*pi/a2
+
+    b = 2 * g_ewald 
+    b2 = b * b
+    kmax2 = kmax * kmax
+    pi2 = pi * pi
+
+    s = 0 # running total
+
+    # Sum over the (+,+,+) octant, then multiply the result by 8.
+    for k in list(product(range(kmax + 1), repeat=3))[1:]:
+        k2 = k[0]*k[0] + k[1]*k[1] + k[2]*k[2]
+        if k2 <= kmax2:
+            q2 = 4 * pi2 * k2 # squared wavenumber
+            t = exp(-(q2 + a2) / b2) / (q2 + a2)
+            for i in range(3):
+                # Correct for overcounting when k is on an axial plane. 
+                if k[i] == 0:
+                    t /= 2
+            s += t
+    # This is where we multiply by 8.
+    # (also need a 4*pi because we are using Gaussian units)
+    s *= 32 * pi 
+
+    # See last line of eqn (2.19) of Salin and Caillol.
+    # (Note sign error in final term.)
+    s -= b * exp(-a2 / b2) / sqrt(pi)
+    s += a * erfc(a / b)
+    if a > 0.001:
+        s += 4 * pi * (exp(-a2 / b2) - 1) / a2
+    else: # If a is small, use expansion about a = 0.
+        s += -4*pi/b2 + 2*pi*a2/(b2*b2) - 2*pi*a2*a2/(3*b2*b2*b2)
+
+    return s
